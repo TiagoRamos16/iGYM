b1b5d933f83bacadea96bda08991adfcd5ff6240
Mauricio <mauricio.gouveia1990@gmail.com>
2019-02-15 16:51:59 +0000

    teste conta

--- application/controllers/Utilizador.php
+++ application/controllers/Utilizador.php
@@ -32,7 +32,7 @@ class Utilizador extends CI_Controller {
 	public function login()
 	{
 		$this->load->view('templates/header');
-		// $this->load->view('templates/nav');
+		$this->load->view('templates/nav');
 		$this->load->view('Utilizador/login');
 		$this->load->view('templates/footer');
 
