10358792ba257bff490da1f370f4997ba9cabc4e
Mauricio <mauricio.gouveia1990@gmail.com>
2019-02-15 16:25:24 +0000

    funcionar inicio

--- application/controllers/Utilizador.php
+++ application/controllers/Utilizador.php
@@ -3,6 +3,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Utilizador extends CI_Controller {
 
+	public function __construct()
+	{
+	/*call CodeIgniter's default Constructor*/
+    parent::__construct();
+    
+	// $this->load->model('automovel_m');
+ 	$this->load->library('session'); 	// -------> com isto iniciamos a biblioteca para pudermos utilizar sessões <--------
+ 	$this->load->library('form_validation'); // -------> com isto iniciamos a biblioteca para pudermos validar os form <--------
+ 	$this->load->helper('security'); // -------> com isto iniciamos a biblioteca para pudermos validar nos form o XSS-clean <--------
+    }
+
+	// public function view($page = 'home')
+	// {
+	// 		if ( ! file_exists(APPPATH.'views/Utilizador/'.$page.'.php'))
+	// 		{
+	// 				// Whoops, we don't have a page for that!
+	// 				show_404();
+	// 		}
+	
+	// 		$data['title'] = ucfirst($page); // Capitalize the first letter
+	
+	// 		$this->load->view('templates/header', $data);
+	// 		$this->load->view('Utilizador/'.$page, $data);
+	// 		$this->load->view('templates/footer', $data);
+	// }
+
 	public function login()
 	{
 		$this->load->view('templates/header');
