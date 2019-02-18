88349398afac8298d57a1a832a2f12131c6f72e2
Tiago Ramos <tiago.vramos16@gmail.com>
2019-02-18 15:38:08 +0000

    s

--- application/controllers/Utilizador.php
+++ application/controllers/Utilizador.php
@@ -5,7 +5,6 @@ class Utilizador extends CI_Controller {
 
 	public function __construct()
 	{
-<<<<<<< HEAD
 		parent::__construct();
 
 		$this->load->library('session');
@@ -13,31 +12,6 @@ class Utilizador extends CI_Controller {
 		// $this->load->library('security');
 	}
 
-=======
-	/*call CodeIgniter's default Constructor*/
-    parent::__construct();
-    
-	// $this->load->model('automovel_m');
- 	$this->load->library('session'); 	// -------> com isto iniciamos a biblioteca para pudermos utilizar sessões <--------
- 	$this->load->library('form_validation'); // -------> com isto iniciamos a biblioteca para pudermos validar os form <--------
- 	$this->load->helper('security'); // -------> com isto iniciamos a biblioteca para pudermos validar nos form o XSS-clean <--------
-    }
-
-	// public function view($page = 'home')
-	// {
-	// 		if ( ! file_exists(APPPATH.'views/Utilizador/'.$page.'.php'))
-	// 		{
-	// 				// Whoops, we don't have a page for that!
-	// 				show_404();
-	// 		}
-	
-	// 		$data['title'] = ucfirst($page); // Capitalize the first letter
-	
-	// 		$this->load->view('templates/header', $data);
-	// 		$this->load->view('Utilizador/'.$page, $data);
-	// 		$this->load->view('templates/footer', $data);
-	// }
->>>>>>> c55fc9624cd8502eba008d4e8c40cec8ecd65ce8
 
 	public function login()
 	{
