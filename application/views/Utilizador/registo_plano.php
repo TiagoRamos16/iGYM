a97e9e9d541773a6df426503b5ac440a558b2600
Mauricio <mauricio.gouveia1990@gmail.com>
2019-02-19 10:32:31 +0000

    plano

---
+++ application/views/Utilizador/registo_plano.php
@@ -0,0 +1,56 @@
+<!-- details card section starts from here -->
+<section class="details-card">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-4">
+                <div class="prices">
+                    <ul>
+                        <li class="title">Estudante</li>
+                        <li class="price">17,00€*</li>
+                        <li class="option-available">Segunda a Domingo</li>
+                        <li class="option-available">Cardiofit &amp; musculação</li>
+                        <li class="option-unavailable">BOX Crosstraining</li>
+                        <li class="option-available">Aulas de Grupo</li>
+                        <li><strong>*valor para Contrato anual</strong></li>
+                        <li>Débito direto:<strong>19.49€</strong></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="prices">
+                    <ul>
+                        <li class="title">Estudante</li>
+                        <li class="price">17,00€*</li>
+                        <li class="option-available">Segunda a Domingo</li>
+                        <li class="option-available">Cardiofit &amp; musculação</li>
+                        <li class="option-unavailable">BOX Crosstraining</li>
+                        <li class="option-available">Aulas de Grupo</li>
+                        <li><strong>*valor para Contrato anual</strong></li>
+                        <li>Débito direto:<strong>19.49€</strong><br>
+                            Pagamento ao balcão: <strong>22.00€</strong></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://placeimg.com/380/230/animals" alt="">
+                        <span><h4>heading2</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3>Heading2</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, voluptatum! Dolor quo, perspiciatis
+                            voluptas totam</p>
+                            <a href="#" class="btn-card">Read</a>   
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
