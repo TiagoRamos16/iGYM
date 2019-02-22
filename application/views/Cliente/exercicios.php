2fe17c88a6d2746e9456d560e2b44940b3110648
Mauricio <mauricio.gouveia1990@gmail.com>
2019-02-22 17:39:23 +0000

    exercicios plano

--- application/views/Cliente/exercicios.php
+++ application/views/Cliente/exercicios.php
@@ -9,11 +9,7 @@
             foreach($listaExercicios as $row)
             { ?>
             
-            <?php 
-            // echo form_open("cliente/exercicioSelecionado/$row['id']"); 
-            ?>
-
-            <a class="exercicios-link" href="<?= base_url('cliente/exercicioSelecionado/'.$row['id'])?>">
+            <a class="exercicios-link" href="#edit<?php echo $row['id'];?>" data-title="Edit" data-toggle="modal">
                 <div class="col-md-3">
                     <div class="exercicios-item text-center">
                         <div class="info">
@@ -29,18 +25,52 @@
                     </div>
                 </div>
             </a>
-            
+
+            <!-- https://www.spotebi.com/fitness-tips/best-back-exercises-posture-tone-strength/ -->
+
+            <div class="modal fade" id="edit<?php echo $row['id'];?>" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <?php echo form_open('cliente/exercicioSelecionado/'.$row['id'],'class="form form-msn"'); ?>
+                        <div class="modal-header" id="titulo-exercicios">
+                            <button type="button" id="fechar-modal" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title"> <?php echo $row['nome'] ?> </h4>
+                        </div>
+                        <div class="modal-body">
+
+                            <div class="form-group">
+                                <img class="img-exercicio" src="<?php echo base_url("assets/img/exercicios/".$row['foto']) ?>" alt="">
+                            </div>
+                            <div class="form-group">
+                                <label for="descricao-exercicio">Descrição</label>
+                                <p>
+                                    <?php echo $row['descricao'] ?>
+                                </p>
+                            </div>
+                            <div class="form-group">
+                                <label for="descricao-exercicio">Tipo de Exercício:</label><?php echo ' '.$row['tipo_exercicio'] ?>
+                            </div>
+                            <div class="form-group">
+                                <label for="descricao-exercicio">Dificuldade:</label><?php echo ' '.$row['dificuldade'] ?>
+                            </div>
+                            <div class="form-group">
+                                <label for="descricao-exercicio">Duração:</label><?php echo ' '.$row['duracao'].' '.$row['tipo_duracao'] ?>
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <input type="submit" class="btn btn-primary" value="Adicionar ao seu plano" name="editarPerfil">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
             <?php 
-            // echo form_close();
+             } //  <!-- foreach -->
+            ?> 
 
-             } 
-            
-            ?> <!-- foreach -->
-            
-
-            
-            
-                
         </div>
         <!-- /.row -->
     </div>
